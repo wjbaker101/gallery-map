530390b9d5fc412ea7917859bf3dfe7013b15f4a
wjbaker101 <willbaker10198@gmail.com>
2022-06-05 19:23:12 +0100

    Move album details into component to allow logic based on a single album

--- frontend/src/view/admin/AdminView.vue
+++ frontend/src/view/admin/AdminView.vue
@@ -5,50 +5,25 @@
             <IconComponent icon="plus" gap="right" />
             <span>Album</span>
         </ButtonComponent>
-        <details class="album-container" :key="album.id" v-for="album in albums">
-            <summary class="flex gap">
-                <div class="flex-auto">
-                    <IconComponent class="open" icon="arrow-triangle-right" />
-                    <IconComponent class="closed" icon="arrow-triangle-down" />
-                </div>
-                <div>{{ album.title }}</div>
-                <div class="flex-auto">({{ album.photos.length }})</div>
-            </summary>
-            <VueDraggable class="photos-container flex gap-small" tag="div" v-model="album.photos" item-key="id">
-                <template #header>
-                    <div class="add-photo photo-container flex-auto flex align-items-center" @click="onAddPhoto(album)">
-                        <div class="flex-auto text-centered">+<br>Photo</div>
-                    </div>
-                </template>
-                <template #item="{element}">
-                    <div class="photo-container flex-auto">
-                        <img :src="element.thumbnailUrl">
-                        <DeleteButtonComponent class="delete" onlyIcon @delete="onDeletePhoto(album, element)" />
-                    </div>
-                </template>
-            </VueDraggable>
-        </details>
+        <AlbumDetailsComponent :key="album.id" v-for="album in albums" :album="album" />
     </CardComponent>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
-import VueDraggable from 'vuedraggable';
 
 import CardComponent from '@/component/CardComponent.vue';
+import AlbumDetailsComponent from '@/view/admin/component/AlbumDetailsComponent.vue';
 
 import { useAppData } from '@/use/appData.use';
 import { useEvents } from '@/use/events.use';
-
-import { IAlbum } from '@/model/album.model';
-import { IPhoto } from '@/model/photo.model';
 
 export default defineComponent({
     name: 'AdminView',
 
     components: {
-        VueDraggable,
         CardComponent,
+        AlbumDetailsComponent,
     },
 
     setup() {
@@ -70,19 +45,6 @@ export default defineComponent({
                     name: 'new-album',
                 });
             },
-
-            onAddPhoto(album: IAlbum) {
-                events.publish('show-modal', {
-                    name: 'new-photo',
-                    props: {
-                        album,
-                    },
-                });
-            },
-
-            async onDeletePhoto(album: IAlbum, photo: IPhoto) {
-                await appData.photos.delete(album.id, photo.id);
-            },
         };
     },
 });
@@ -95,84 +57,9 @@ export default defineComponent({
     bottom: 1rem;
     right: 1rem;
 
-    $border-width: 3px;
-
     .add-album {
         display: table;
         margin: 0 auto 1rem auto;
-    }
-
-    .album-container {
-        summary {
-            padding: 0.5rem;
-            margin-bottom: 0.5rem;
-            cursor: pointer;
-            border: $border-width dashed transparent;
-            user-select: none;
-
-            @include vars.border-radius;
-            @include vars.shadow-small;
-
-            &:hover {
-                border: $border-width dashed #176bc0;
-            }
-        }
-
-        .closed {
-            display: none;
-        }
-
-        &[open] {
-            .closed {
-                display: unset;
-            }
-
-            .open {
-                display: none;
-            }
-        }
-
-        & + .album-container {
-            margin-top: 1rem;
-        }
-    }
-
-    .photos-container {
-        flex-wrap: wrap;
-    }
-
-    .photo-container {
-        width: 160px;
-        height: 160px;
-        position: relative;
-        border: $border-width dashed transparent;
-
-        @include vars.border-radius;
-
-        img {
-            @include vars.border-radius;
-            @include vars.shadow-small;
-        }
-
-        &:hover {
-            border: $border-width dashed #176bc0;
-        }
-
-        .delete {
-            position: absolute;
-            bottom: 0.5rem;
-            right: 0.5rem;
-        }
-    }
-
-    .add-photo {
-        cursor: pointer;
-
-        @include vars.shadow-small;
-
-        & > div {
-            margin: auto;
-        }
     }
 }
 </style>
---
+++ frontend/src/view/admin/component/AlbumDetailsComponent.vue
@@ -0,0 +1,150 @@
+<template>
+    <details class="album-details">
+        <summary class="flex gap">
+            <div class="flex-auto">
+                <IconComponent class="open" icon="arrow-triangle-right" />
+                <IconComponent class="closed" icon="arrow-triangle-down" />
+            </div>
+            <div>{{ album.title }}</div>
+            <div class="flex-auto">({{ album.photos.length }})</div>
+        </summary>
+        <VueDraggable class="photos-container flex gap-small" tag="div" v-model="album.photos" item-key="id">
+            <template #header>
+                <div class="add-photo photo-container flex-auto flex align-items-center" @click="onAddPhoto(album)">
+                    <div class="flex-auto text-centered">+<br>Photo</div>
+                </div>
+            </template>
+            <template #item="{element}">
+                <div class="photo-container flex-auto">
+                    <img :src="element.thumbnailUrl">
+                    <DeleteButtonComponent class="delete" onlyIcon @delete="onDeletePhoto(album, element)" />
+                </div>
+            </template>
+        </VueDraggable>
+    </details>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import VueDraggable from 'vuedraggable';
+
+import { useAppData } from '@/use/appData.use';
+import { useEvents } from '@/use/events.use';
+
+import { IAlbum } from '@/model/album.model';
+import { IPhoto } from '@/model/photo.model';
+
+export default defineComponent({
+    name: 'AlbumDetailsComponent',
+
+    props: {
+        album: {
+            type: Object as PropType<IAlbum>,
+            required: true,
+        },
+    },
+
+    components: {
+        VueDraggable,
+    },
+
+    setup() {
+        const appData = useAppData();
+        const events = useEvents();
+
+        return {
+            onAddPhoto(album: IAlbum) {
+                events.publish('show-modal', {
+                    name: 'new-photo',
+                    props: {
+                        album,
+                    },
+                });
+            },
+
+            async onDeletePhoto(album: IAlbum, photo: IPhoto) {
+                await appData.photos.delete(album.id, photo.id);
+            },
+        };
+    },
+});
+</script>
+
+<style lang="scss">
+@use '@/style/variables' as vars;
+
+.album-details {
+    $border-width: 3px;
+
+    summary {
+        padding: 0.5rem;
+        margin-bottom: 0.5rem;
+        cursor: pointer;
+        border: $border-width dashed transparent;
+        user-select: none;
+
+        @include vars.border-radius;
+        @include vars.shadow-small;
+
+        &:hover {
+            border: $border-width dashed #176bc0;
+        }
+    }
+
+    .closed {
+        display: none;
+    }
+
+    &[open] {
+        .closed {
+            display: unset;
+        }
+
+        .open {
+            display: none;
+        }
+    }
+
+    & + .album-container {
+        margin-top: 1rem;
+    }
+
+    .photos-container {
+        flex-wrap: wrap;
+    }
+
+    .photo-container {
+        width: 160px;
+        height: 160px;
+        position: relative;
+        border: $border-width dashed transparent;
+
+        @include vars.border-radius;
+
+        img {
+            @include vars.border-radius;
+            @include vars.shadow-small;
+        }
+
+        &:hover {
+            border: $border-width dashed #176bc0;
+        }
+
+        .delete {
+            position: absolute;
+            bottom: 0.5rem;
+            right: 0.5rem;
+        }
+    }
+
+    .add-photo {
+        cursor: pointer;
+
+        @include vars.shadow-small;
+
+        & > div {
+            margin: auto;
+        }
+    }
+}
+</style>
